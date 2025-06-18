e219cc8d06758cfa6e519e25510808dd725ef195
Jahnavi Mogarala <mogaralajahnavi9@gmail.com>
2025-06-18 19:34:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener("scroll", function () {
-  const nav = document.getElementById("JKUAD6140-nav");
-  if (window.scrollY > 50) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-  }
-});
