337f30ac33144faf9910f828cfa7972b1b867e94
Jahnavi Mogarala <mogaralajahnavi9@gmail.com>
2025-06-18 19:20:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("scroll", function () {
+  const nav = document.getElementById("JKUAD6140-nav");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
